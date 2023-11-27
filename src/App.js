d679ccc6f9e6344f9f9f351ea1840448a4e27d63
maxih4 <handkemax@gmail.com>
2023-11-27 19:16:21 +0100

    updated icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 import {AuthProvider} from 'react-auth-kit'
 import RoutesComponent from './Routes';
 import refreshApi from "./refreshApi";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./css/App.css"
 
 function App() {

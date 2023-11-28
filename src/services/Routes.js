a0850fb462a388bb9f3d0754f1d9c807b2aef1aa
Max Handke <handkemax@gmail.com>
2023-11-28 13:19:50 +0100

    Fixed Routing, added "Create Comment"

--- src/services/Routes.js
+++ src/services/Routes.js
@@ -1,33 +1,43 @@
 import React from 'react'
-import { RequireAuth } from 'react-auth-kit'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {RequireAuth, useIsAuthenticated} from 'react-auth-kit'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import UserPanel from '../components/UserPanel'
 import SingleEvent from "../components/SingleEvent";
 import Navbar from "../components/Navbar";
 import Register from "../components/Register";
+import CreateEvent from "../components/CreateEvent";
+
+
 
 const RoutesComponent = () => {
-  return (
+    const PrivateRoute = ({ Component }) => {
+        const isAuthenticated = useIsAuthenticated();
+        const auth = isAuthenticated();
+        return auth ? <Component /> : <Navigate to="/login" />;
+    };
 
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path={'/'} element={<Home/>}/>
-        <Route path={'/login' } element={<Login/>}/>
-        <Route path={'/user'} element={
-          <RequireAuth loginPath={'/login'}>
-            <UserPanel/>
-          </RequireAuth>
-        }/>
-          <Route path={'/register'} element={<Register/>}/>
-        <Route path={'/event/:id'} element={<SingleEvent/>}/>
 
-      </Routes>
+    return (
 
-    </BrowserRouter>
-  )
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/user'} element={
+                    <RequireAuth loginPath={'/login'}>
+                        <UserPanel/>
+                    </RequireAuth>
+                }/>
+                <Route path={'/register'} element={<Register/>}/>
+                <Route path={'/event/:id'} element={<SingleEvent/>}/>
+                <Route path={'/createEvent'} element={<PrivateRoute Component={CreateEvent}/>}/>
+            </Routes>
+
+        </BrowserRouter>
+    )
 }
 
 export default RoutesComponent

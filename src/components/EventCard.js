4ab4d9bab2732783292b94aee0a403fe0e5561ac
Max Handke <handkemax@gmail.com>
2023-12-15 00:37:31 +0100

    updated UI, implemented Postal Code

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import DOMPurify from "dompurify";
-import {HtmlEditor} from "react-simple-wysiwyg";
+
 import HTMLEllipsis from "react-lines-ellipsis/lib/html.modern.mjs";
 import LinesEllipsis from "react-lines-ellipsis";
-import {Space} from "antd";
-
 
 
 
 function EventCard(props) {
-    const handleReflow=(e)=>{
-    return e.clamped
-}
+    const handleReflow = (e) => {
+        return e.clamped
+    }
 
+    let date = new Date(props.event.startDate);
     return (
 
         <>
 
-            <div className="card">
-                <div className="row g-0 row-cols-1">
+            <div className="card eventCard">
+                <div className="row g-0 row-cols-1 ">
 
-                    <div className="text-center col-2 rounded"
-                         style={{backgroundColor: "#77BB41", backgroundSize: "cover", color:"white"}}>
+                    <div className="text-center col-2"
+                         style={{backgroundColor: "#77BB41", backgroundSize: "cover", color: "white"}}>
                         <br/>
                         <br/>
-                        <h2 style={{fontFamily: "Judson", fontSize: "80px"}}>{new Date(props.event.startDate).toLocaleString("de-De",{day: "numeric"})}</h2>
+                        <h2 style={{
+                            fontFamily: "Judson",
+                            fontSize: "80px"
+                        }}>{date.toLocaleString("de-De", {day: "numeric"})}</h2>
                         <br/>
-                        <h2 style={{fontFamily: "Kameron", fontWeight:"bold", fontSize:"81px"}}>{new Date(props.event.startDate).toLocaleString("de-De", {month: "short"})}</h2>
+                        <h2 style={{
+                            fontFamily: "Kameron",
+                            fontWeight: "bold",
+                            fontSize: "81px"
+                        }}>{date.toLocaleString("de-De",{month:"short"} )}</h2>
 
                     </div>
 
 
-                    <div className="card-body col-10" >
-                        <h5 className="card-title">{props.event.title}</h5>
+                    <div className="card-body col-10 text-center">
+
+                        <h3 className="card-title"
+                            style={{fontFamily: "Outfit", fontSize: "64px"}}> <LinesEllipsis text={props.event.title}>
+
+
+                        </LinesEllipsis></h3>
 
 
                         <HTMLEllipsis
@@ -44,9 +55,20 @@ function EventCard(props) {
                         />
 
                         <br/>
+                        <div className="d-flex flex-row justify-content-around" style={{fontFamily:"Inter",fontWeight:"Bold"}}>
+                            <div className="p-2">{props.event.streetName + " "+ props.event.houseNumber}
+                                <br/>
+                                {props.event.city}
+                            </div>
+                            <p className="p-2" style={{fontFamily:"Inter",fontWeight:"Bold"}}>{date.toLocaleString("de-DE",{hour:"2-digit"})}</p>
+                            <div className="p-2">
+                                <button className="" style={{borderRadius:"61px",backgroundColor: "#77BB41",borderColor: "#77BB41",width:"255px",
+                                    borderStyle: "solid"}}>
+                                    <Link to={"/event/" + props.event.id} style={{textDecoration: "none", color:"black",fontFamily:"Outfit", fontWeight:"bold",fontSize:"27px"}}> Jetzt
+                                        teilnehmen </Link></button>
+                            </div>
+                        </div>
 
-                        <Link to={"/event/" + props.event.id} className="btn btn-primary">Go to Event with
-                            id: {props.event.id}</Link>
                     </div>
                 </div>
             </div>
